50f12611f1bfd03273243416be2c30848e7d9ec0
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-01-31 16:49:11 -0300

    feito a redução de código desnecessários

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React, { useState } from 'react';
+import React, { 
+    useState 
+} from 'react';
 import { Link } from 'react-router-dom';
 import { Nav } from '../layout/Navbar-styled';
+
 export default function Navbar() 
 {
     const [icon, setIcon] = useState(false)
     const toggleIcon = () => setIcon(!icon)
+
     return (
         <Nav>
-            <div className="container_navbar">
-                <Link className='logo' to="/codelandia-9/">Portifólio</Link>
+            <Link className='logo' to="/codelandia-9/">Portifólio</Link>
 
-                <div onClick={toggleIcon} className="menu_icon">
-                    <i className={icon ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                
-                <ul className={icon ? 'open' : 'close'}>
-                    <li><a onClick={toggleIcon} href="#about">Sobre mim</a></li>
-                    <li><a onClick={toggleIcon} href="#projects">Projetos</a></li>
-                    <li><a onClick={toggleIcon} href="#services">Serviços</a></li>
-                    <li><a onClick={toggleIcon} href="#skills">Minhas skills</a></li>
-                </ul>
+            <div onClick={toggleIcon} className="menu_icon">
+                <i className={icon ? 'fas fa-times' : 'fas fa-bars'} />
             </div>
+
+            <ul className={icon ? 'open' : 'close'}>
+                <li><a onClick={toggleIcon} href="#about">Sobre mim</a></li>
+                <li><a onClick={toggleIcon} href="#projects">Projetos</a></li>
+                <li><a onClick={toggleIcon} href="#services">Serviços</a></li>
+                <li><a onClick={toggleIcon} href="#skills">Minhas skills</a></li>
+            </ul>
         </Nav>
     )
 }
